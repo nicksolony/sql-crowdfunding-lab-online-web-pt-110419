b7eec24e63a5134e14976e20df800387913a85c8
Nick Solonyy <nicksolony@gmail.com>
2020-01-30 04:14:57 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,10 @@
 insert into projects (title,category,funding_goal, start_date, end_date) values ("name","cat",1000,"june 2020","jan 2021");
+insert into projects (title,category,funding_goal, start_date, end_date) values ("name","cat",1000,"june 2020","jan 2021");
+insert into projects (title,category,funding_goal, start_date, end_date) values ("name","cat",1000,"june 2020","jan 2021");
+insert into projects (title,category,funding_goal, start_date, end_date) values ("name","cat",1000,"june 2020","jan 2021");
+insert into projects (title,category,funding_goal, start_date, end_date) values ("name","cat",1000,"june 2020","jan 2021");
+insert into projects (title,category,funding_goal, start_date, end_date) values ("name","cat",1000,"june 2020","jan 2021");
+insert into projects (title,category,funding_goal, start_date, end_date) values ("name","cat",1000,"june 2020","jan 2021");
+insert into projects (title,category,funding_goal, start_date, end_date) values ("name","cat",1000,"june 2020","jan 2021");
+insert into projects (title,category,funding_goal, start_date, end_date) values ("name","cat",1000,"june 2020","jan 2021");
+insert into projects (title,category,funding_goal, start_date, end_date) values ("name","cat",1000,"june 2020","jan 2021");
